8f97bcc8661d56ea7fefab598905d04cc0a377f9
fgxut <fgxut1206@gmail.com>
2019-11-01 18:50:10 +0900

    #48 ランダムのさいころを追加

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice
+
+var x = document.querySelector('#score-0').textContent;
+
+document.querySelector('.dice').style.display = 'none';
