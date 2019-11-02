0b72f64d0a7cd0775526efed2eb217c6e4bf539b
fgxut <fgxut1206@gmail.com>
2019-11-02 23:29:01 +0900

    #49 初期値を0に変更

--- app.js
+++ app.js
@@ -15,10 +15,26 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice
-
-var x = document.querySelector('#score-0').textContent;
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the results
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update the round soore IF the rolled number was NOT a 1
+});
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice
+// var x = document.querySelector('#score-0').textContent;
